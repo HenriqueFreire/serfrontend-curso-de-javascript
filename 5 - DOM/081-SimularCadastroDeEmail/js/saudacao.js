731d6152cab6638ea3c946f787cf4ac50ae2da31
HenriqueFreire <henriquefreire@outlook.com.br>
2022-12-07 23:40:09 -0300

    Aprendi à manipular elementos HTML através da DOM

---
+++ 5 - DOM/081-SimularCadastroDeEmail/js/saudacao.js
@@ -0,0 +1,10 @@
+
+
+function sendEmail() {
+  const txtEmail = document.querySelector('#txtEmail').value
+  const msgFeedBack = document.querySelector('#newsletterFeedback')
+    msgFeedBack.textContent = `O e-mail: ${txtEmail} foi cadastradocom sucesso!`
+
+}
+  
+
